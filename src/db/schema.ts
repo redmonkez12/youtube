0ab8fbda406c09ef57e9f175dd6fbc6e12c4aa45
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2025-07-12 20:06:28 +0200

    Add db

---
+++ src/db/schema.ts
@@ -0,0 +1,10 @@
+import { pgTable, text, timestamp, uniqueIndex, uuid } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+    id: uuid("id").primaryKey().defaultRandom(),
+    clerkId: text("clerk_id").notNull().unique(),
+    name: text("name").notNull(),
+    imageUrl: text("image_url").notNull(),
+    createdAt: timestamp("created_at").notNull().defaultNow(),
+    updateAt: timestamp("updated_at").notNull().defaultNow(),
+}, (t) => [uniqueIndex("clerk_id_idx").on(t.clerkId)]);
