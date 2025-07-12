078a3ca7ff1407362b7ad0b1332dee5f190abe14
Tomas Svojanovsky <tomas.svojanovsky33@gmail.com>
2025-07-12 21:21:11 +0200

    Add webhook

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,13 +3,11 @@ import { config } from 'dotenv';
 
 config({ path: '.env.local' });
 
-console.log(process.env.DATABASE_URL);
-
 export default defineConfig({
-    out: './drizzle',
-    schema: './src/db/schema.ts',
-    dialect: 'postgresql',
-    dbCredentials: {
-        url: process.env.DATABASE_URL!,
-    },
+  out: './drizzle',
+  schema: './src/db/schema.ts',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
 });
